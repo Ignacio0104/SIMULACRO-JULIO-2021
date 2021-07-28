d0c8c4468596b2fd96709818d4457f27d05ad6ff
Ignacio0104 <ignacio.smirlian@hotmail.com>
2021-07-28 20:44:33 -0300

    Ejercicio 03

--- tres.js
+++ tres.js
@@ -25,8 +25,25 @@ Ejercicio -3
 function mostrar()
 {
 	var comenzar;
+	var vacunaCaliente;
+	var temperaturaMasAlta;
+	var primerIngreso;
+	var personaCaliente;
+	var mujeresSputnik;
+	var noBinarioVacuna;
+	var astraCaliente;
+	var totalHombresFrios;
+	var edadHombresFrios;
+	var promedioEdadHombresFrios;
 
-	comenzar=prompt("Desea comenzar el ingreso de datos? y (si) o n (no)")
+
+	comenzar=prompt("Desea comenzar el ingreso de datos? y (si) o n (no)");
+	primerIngreso=true;
+	mujeresSputnik=0;
+	noBinarioVacuna=0;
+	astraCaliente=0;
+	totalHombresFrios=0;
+	edadHombresFrios=0;
 
 	while(comenzar=="y")
 	{
@@ -38,6 +55,80 @@ function mostrar()
 
 
 		nombreIngresado=prompt("Ingrese el nombre");
+
 		edadIngresada=prompt("Ingrese la edad");
+		edadIngresada=parseInt(edadIngresada);
+
+		generoIngresado=prompt("Ingrese su genero f (femenino), m (masculino) o nb (no binario)");
+
+		while(generoIngresado!="f"&&generoIngresado!="m"&&generoIngresado!="nb")
+		{
+			generoIngresado=prompt("Error, dato ingresado inválido. Vuelva a ingresarlo");
+		}
+
+		vacunaIngresada=prompt("Ingrese la vacuna (SputnikV, AstraZeneca, Otra");
+
+		while(vacunaIngresada!="SputnikV"&&vacunaIngresada!="AstraZeneca"&&vacunaIngresada!="Otra")
+		{
+			vacunaIngresada=prompt("Error, dato ingresado inválido. Vuelva a ingresarlo");
+		}
+
+		temperaturaCorporal=prompt("Ingrese la temperatura corporal (durante la primera noche)");
+		temperaturaCorporal=parseInt(temperaturaCorporal);
+
+		if(temperaturaCorporal>temperaturaMasAlta||primerIngreso)
+		{
+			temperaturaMasAlta=temperaturaCorporal;
+			vacunaCaliente=vacunaIngresada;
+			personaCaliente=nombreIngresado;
+			primerIngreso=false;
+		}
+
+		switch(generoIngresado)
+		{
+			case "f":
+				if(vacunaIngresada=="SputnikV")
+				{
+					mujeresSputnik=mujeresSputnik+1;
+				}
+				break;
+			case "nb":
+				if(vacunaIngresada=="Otra"||vacunaIngresada=="AstraZeneca")
+				{
+					noBinarioVacuna=noBinarioVacuna+1;
+				}
+				break;
+			case "m":
+				if(vacunaIngresada=="SputnikV"&&temperaturaCorporal<38)
+				{
+					totalHombresFrios=totalHombresFrios+1;
+					edadHombresFrios=edadHombresFrios+edadIngresada;
+				}
+		}
+
+		if(vacunaIngresada=="AstraZeneca"&&temperaturaCorporal>38)
+		{
+			astraCaliente=astraCaliente+1;
+		}
+
+		comenzar=prompt("Desea ingresar otro dato? y (si) o n (no)");
+
 	}
+
+	promedioEdadHombresFrios=edadHombresFrios/totalHombresFrios;
+
+	alert("El nombre de la persona con mayor temperatura es " + personaCaliente 
+	+ " y la vacuna que se dio es la " + vacunaCaliente);
+
+	alert("La cantidad de mujeres que recibieron la vacuna SputnikV son " + mujeresSputnik);
+
+	alert("La cantidad de persona con género no binario que recibieron la vacuna AstraZeneca u otra son " + 
+	noBinarioVacuna);
+
+	alert("La cantidad de personas que se aplicaron la vacuna AstraZeneca y presentaron una temperatura mayor a 38 es " + 
+	astraCaliente);
+
+	alert("El promedio de edad de los hombres que se aplicaron la Sputnik y no presentaron fiebre es de " +
+	promedioEdadHombresFrios + " años");
 }
+
